bf11676e5ea130f3f3b4108ca079a278420127f8
Shoplinka1 <phacodc0@gmail.com>
2026-09-13 02:52:57 +0100

    Add files via upload

---
+++ supabase.ts
@@ -0,0 +1,12 @@
+import { createClient } from '@supabase/supabase-js';
+
+const SUPABASE_URL = 'https://fyxywsocbfcnbprlknfp.supabase.co';
+const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_j4psJxTD4_GifcIWhoFHvg_YINJGDPH';
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY, {
+  auth: {
+    persistSession: true,
+    autoRefreshToken: true,
+    detectSessionInUrl: true,
+  },
+});
